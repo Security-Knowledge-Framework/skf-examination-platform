ea8d962d8eef817ed10e663e5048aa216f597605
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-11-23 21:02:54 -0500

    register and login style

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -33,10 +33,11 @@ export function LoginInpout() {
   }
 
   return (
-    <>
+    <div class="login-form">
       <h3>Sign in to your account</h3>
       <form onSubmit={handleSubmit} action="" method="post">
-        <label for="email">Username or email</label>
+      <div class="item-input">
+        <label for="email">Username or email:</label>
         <input
           type="text"
           name="email"
@@ -44,8 +45,10 @@ export function LoginInpout() {
           value={form.email}
           onChange={updateFormField("email")}
         />
+      </div>
 
-        <label for="password">Password</label>
+      <div class="item-input">
+        <label for="password">Password:</label>
         <input
           type="password"
           name="password"
@@ -53,6 +56,10 @@ export function LoginInpout() {
           value={form.password}
           onChange={updateFormField("password")}
         />
+      </div>
+        
+
+        
         <Show when={error()}>
           <div class="error">{error}</div>
         </Show>
@@ -60,20 +67,32 @@ export function LoginInpout() {
           <div class="message">{message}</div>
         </Show>
         <a href="/">Forgot Password?</a>
-        <input type="submit" value="Sign In" />
+        <input class="btn" type="submit" value="Sign In" />
       </form>
-      <p>Or sign in with</p>
-      <button class="sign-with" onclick={loginWithGithub}>
-        Github
-      </button>
-      <button class="sign-with" onclick={loginWithGoogle}>
-        Google
-      </button>
-      <div class="new-user">
-        <span>
-          New user? <a href="/register">Register</a>
-        </span>
+      <div class="container-sign-with">
+        <div class="title">
+          <div> </div>
+          <p>Or sign in with</p>
+          <div> </div>
+        </div>
+        <div class="list-social">
+          <button class="sign-with" onclick={loginWithGithub}>
+            Github
+          </button>
+          <button class="sign-with" onclick={loginWithGoogle}>
+            Google
+          </button>
+        </div>
+        <svg height="10" width="100%">
+          <line
+            x1="0"
+            y1="0"
+            x2="100%"
+            y2="0"
+            style="stroke:rgb(160,160,160);stroke-width:2"
+          />
+        </svg>
       </div>
-    </>
+    </div>
   );
 }
