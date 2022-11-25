215049aca7d370b18b778b2b3581666917a872b1
Antony Faris <acp325@gmail.com>
2022-11-25 18:34:22 -0500

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), svelte()]
+  integrations: [solidJs(), svelte()],
 });
